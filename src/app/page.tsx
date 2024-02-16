568158c756742b217c82375b00f85970a0f3ccb0
asif-langrah <ak.development.pk@gmail.com>
2024-02-16 16:59:18 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 const page = () => {
   return (
     
-       
+       <div>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         The Joke Tax Chronicles
       </h1>
-    
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+      The People of the Kingdom
+    </h2>
+    </div>
   )
 }
 
